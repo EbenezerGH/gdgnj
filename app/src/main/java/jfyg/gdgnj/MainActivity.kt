68beaecd76a86c0ea6c6a3d35662457f451c4359
Ebenezer Ackon <ebenezer.ackon@viacom.com>
2017-11-21 19:03:15 -0500

    skkkkkrraaaaaa

---
+++ app/src/main/java/jfyg/gdgnj/MainActivity.kt
@@ -0,0 +1,30 @@
+package jfyg.gdgnj
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+/*
+String[] names = new String[] {
+        "Annmaria", "Hallsy", "Alyse", "Greggory", "Kelly", "Silvanus", "Addy", "Tatiana", "Wandis",
+        "Courtnay", "Huntlee", "Joshuah", "Ericka", "Torrance", "Allan", "Kristina", "Merci",
+        "Corliss", "Elfrida", "Gerald", "Ronna", "Ambrosius", "Sianna", "Kliment", "Townsend",
+        "Abner", "Sean", "Iorgos", "Althea", "Fan", "Kiri", "Kendell", "Freddi", "Currie",
+        "Marcello", "Padget", "Arvy", "Patton", "Aime", "Rodrick", "Marysa", "Hobey", "Deb",
+        "Sallyann", "Padraic", "Rhiamon", "Dorise", "Ebonee", "Nicolis", "Valene", "Sauveur",
+        "Raleigh", "Cathi", "Freeman", "Melisent", "Maressa", "Farrand", "Nita", "Petronille",
+        "Orin", "Dick", "Benjamen", "Marieann", "Daile", "Morgun", "Corine", "Danielle", "Crystie",
+        "Emery", "Brittani", "Bennett", "Dulcine", "Lucho", "Margarita", "Merralee", "Idelle",
+        "Iolande", "Cynde", "Dolly", "Adelle", "Rem", "Jimmy", "Daryl", "Vinnie", "Rosalind",
+        "Lonni", "Donna", "Gus", "Gilbertina", "Dottie", "Jason", "Georgy", "Israel", "Gram",
+        "Saidee", "Mirabelle", "Helli", "Emile", "Wallache"
+    };
+ */
